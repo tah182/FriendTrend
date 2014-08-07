ff1199a4b6d6b1b7230bd8e557b87c7e550c3fc0
Tah Tatsumoto <tah1@Tahs-Macbook.local>
2014-08-06 23:52:56 -0600

    Initial Commit

---
+++ FriendTrend/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  FriendTrend
+//
+//  Created by Tah Tatsumoto on 8/6/14.
+//  Copyright (c) 2014 Mad Hat Productions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
---
+++ FriendTrend/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FriendTrend
+//
+//  Created by Tah Tatsumoto on 8/6/14.
+//  Copyright (c) 2014 Mad Hat Productions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
